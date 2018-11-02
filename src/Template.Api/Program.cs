3e3ca2420485672dd04a8cd6c7635dff8854065d
Murilo Beltrame <murilobeltrame@gmail.com>
2018-11-02 10:57:00 -0300

    First Commit

---
+++ src/Template.Api/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Graylog;
+
+namespace Template.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Graylog(new GraylogSinkOptions
+                {
+                    HostnameOrAddress = "localhost", //TODO: fix that with appsettings
+                    Port = 12201 //TODO: fix that with appsettings
+                })
+                .CreateLogger();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
---
+++ src/Template.Api/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Template.Api.Utils.Swagger.SwaggerGen;
+using Template.Api.Utils.Swagger.SwaggerGen.Extensions;
+
+namespace Template.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // This configuration enables HTTP Compression with Gzip
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            });
+
+            services
+                .AddMvc()
+                .AddJsonOptions(options => 
+                {
+                    // This configuration sets optimization for JSON serializing 
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.Formatting = Formatting.None;
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // This configuration adds support for API versioning
+            services
+                .AddMvcCore()
+                .AddVersionedApiExplorer(options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+                    options.SubstituteApiVersionInUrl = true;
+                });
+            services.AddApiVersioning(options => options.ReportApiVersions = true);
+
+            // This configuration enables Swagger generation
+            services.AddSwaggerGen(options =>
+            {
+                var _provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+                foreach (var _description in _provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerDocByApiVersion(_description);
+                }
+                options.OperationFilter<SwaggerDefaultValues>();
+                var _xmlDocFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(_xmlDocFileName);
+
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.RoutePrefix = string.Empty;
+                foreach (var _description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{_description.GroupName}/swagger.json", _description.GroupName.ToUpperInvariant());
+                }
+            });
+            app.UseResponseCompression();
+            app.UseMvc();
+        }
+    }
+}
