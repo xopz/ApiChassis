96632b6ebf2cc29b46e2d6567a6d7e0e752e075b
Murilo Beltrame <murilobeltrame@gmail.com>
2018-12-23 00:31:38 -0200

    new sourced

---
+++ source/simple/Simple.WebApi/Startup.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using static Microsoft.AspNetCore.Mvc.CompatibilityVersion;
+
+namespace Simple.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = true).SetCompatibilityVersion(Latest);
+
+            // Configuring HTTP Response Compression
+            services.AddResponseCompression(
+                options =>
+                {
+                    options.Providers.Add<BrotliCompressionProvider>();
+                    options.Providers.Add<GzipCompressionProvider>();
+                });
+            services.Configure<BrotliCompressionProviderOptions>(
+                options =>
+                {
+                    options.Level = CompressionLevel.Fastest;
+                });
+            services.Configure<GzipCompressionProviderOptions>(
+                options =>
+                {
+                    options.Level = CompressionLevel.Fastest;
+                });
+
+            // Set versioning
+            services.AddApiVersioning(
+                options =>
+                {
+                    // reporting api versions will return the headers "api-supported-versions" and "api-deprecated-versions"
+                    options.ReportApiVersions = true;
+                });
+            services.AddVersionedApiExplorer(
+                options =>
+                {
+                    // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                    // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // resolve the IApiVersionDescriptionProvider service
+                    // note: that we have to build a temporary service provider here because one has not been created yet
+                    using (var serviceProvider = services.BuildServiceProvider())
+                    {
+                        var provider = serviceProvider.GetRequiredService<IApiVersionDescriptionProvider>();
+
+                        // add a swagger document for each discovered API version
+                        // note: you might choose to skip or document deprecated API versions differently
+                        foreach (var description in provider.ApiVersionDescriptions)
+                        {
+                            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                        }
+                    }
+
+                    // add a custom operation filter which sets default values
+                    options.OperationFilter<SwaggerDefaultValues>();
+
+                    // integrate xml comments
+                    options.IncludeXmlComments(XmlCommentsFilePath);
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseResponseCompression();
+            app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(
+                options =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName.ToUpperInvariant());
+                    }
+                    options.RoutePrefix = string.Empty;
+                });
+
+            app.UseMvc();
+        }
+
+        static string XmlCommentsFilePath
+        {
+            get
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                return Path.Combine(AppContext.BaseDirectory, xmlFile);
+            }
+        }
+
+        static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info
+            {
+                Title = "Sample API",
+                Version = description.ApiVersion.ToString(),
+                Description = "A sample application with Swagger, Swashbuckle, and API versioning.",
+                Contact = new Contact { Name = "Bill Mei", Email = "bill.mei@somewhere.com" },
+                TermsOfService = "Shareware",
+                License = new License { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
